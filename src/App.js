efd2214087e986ff9efc6eb5284b709b930506fc
bolisettysuraj <bolisettysuraj@gmail.com>
2024-07-31 16:16:29 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from './component/Layout';
+import LandingPage from './component/Landingpage';
 import MusicPage from './component/Music';
 import About from './component/about';
 import Event from './component/Event';
